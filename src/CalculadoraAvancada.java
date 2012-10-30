565dfbb39c508c9c1b07bf8c7620ca9e648d4254
renan lucas de azevedo <renan_oliveira07@hotmail.com>
2012-10-30 15:51:14 -0300

    yyt

---
+++ src/CalculadoraAvancada.java
@@ -0,0 +1,18 @@
+
+public class CalculadoraAvancada {
+	public double calculaCos (double x) {
+		return Math.cos(x);
+	}
+	public double calculaSen (double x) {
+		return Math.sin(x);
+	}
+	public double calculaTan (double x) {
+		return Math.tan(x);
+	}
+	public double calculaSqrt (double x) {
+		return Math.sqrt(x);
+	}
+	public double calculaPotencia (double x, double y) {
+		return Math.pow(x,y);
+	}
+}
---
+++ src/CalculadoraSimples.java
@@ -0,0 +1,18 @@
+
+public class CalculadoraSimples {
+	public int adicao (int a, int b) {
+		return a+b;
+	}
+	
+	public int subtracao (int a, int b) {
+		return a-b;
+	}
+	
+	public double divisao (double a, double b) {
+		return a/b;
+	}
+	
+	public double multiplicacao (double a, double b) {
+		return a*b;
+	}
+}
